f667a965e87bc401fb049ad6a0058ade13119045
Yak <bigfootmech@gmail.com>
2025-03-20 13:35:02 +0000

    GHI_8-MainMenu: Added main menu with some basic functionality. Currently leads to test scene when you press 'start'

---
+++ main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+var scene_preload = preload("res://test_scene.tscn")
+
+func _on_start_button_pressed():
+	get_node("/root/MainMenu").queue_free()
+	get_tree().get_root().add_child(scene_preload.instantiate())
+
+func _on_options_button_pressed():
+	pass # Replace with function body.
+
+func _on_credits_button_pressed():
+	pass # Replace with function body.
+
+func _on_exit_button_pressed():
+	get_tree().quit()
