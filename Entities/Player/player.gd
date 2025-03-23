9c2c6419688aef71eff88be0f265e8b1ac23bad8
Yak <bigfootmech@gmail.com>
2025-03-23 12:24:35 +0000

    Slight refactor of player script, not sure it makes it any more readable :/

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -6,26 +6,35 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @export var speed = 7000
 @export var JUMP_VELOCITY = 380.0
 
+func _input(event):
+	if event.is_action_pressed("jump"):
+		jump()
+
 func _physics_process(delta):
+	_set_move(delta)
+	_apply_gravity(delta)
+	
+	move_and_slide()
+	
+func _set_move(delta):
 	var input_direction = Input.get_axis("ui_left", "ui_right")
 	velocity.x = input_direction * speed * delta
-	_turn_sprite(velocity.x)
 	
+	_turn_sprite()
+	
+func _apply_gravity(delta):
 	if !is_on_floor():
 		velocity.y += gravity * delta
 	
-	if(Input.is_action_just_pressed("jump") && is_on_floor()):
-		if(Input.is_action_pressed("ui_down")):
-			# Jump Down
+func jump():
+	if(is_on_floor()): # don't jump in the air
+		if(Input.is_action_pressed("ui_down")): # Jump Down
 			position.y += 2
 		else:
-			# Jump
-			velocity.y -= JUMP_VELOCITY
-
-	move_and_slide()
+			velocity.y -= JUMP_VELOCITY # Jump
 	
-func _turn_sprite(dir):
-	if(!%AnimatedSprite2D.flip_h && dir >0):
+func _turn_sprite():
+	if(!%AnimatedSprite2D.flip_h && velocity.x >0):
 		%AnimatedSprite2D.flip_h = true
-	if(%AnimatedSprite2D.flip_h && dir <0):
+	if(%AnimatedSprite2D.flip_h && velocity.x <0):
 		%AnimatedSprite2D.flip_h = false
